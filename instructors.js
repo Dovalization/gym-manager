50e3a33eb6efab0c5e4cd07eb2c539dc7cd16558
Guilherme Doval <doval.guilherme94@gmail.com>
2020-07-24 15:37:48 +0100

    put added

--- instructors.js
+++ instructors.js
@@ -1,20 +1,18 @@
 const fs = require('fs')
 const data = require("./data.json");
 const Intl = require('intl')
-const { age } = require("./utils")
+const { age, date } = require("./utils")
 
 exports.show = (req,res) => {
     const {id} = req.params
 
-    const foundInstructor = data.instructors.find((instructor)=> {
+    const foundInstructor = data.instructors.find((instructor) => {
         return instructor.id == id
     })
 
     if (!foundInstructor) {
         return res.send("Instructor not found")
     }
-
-    
 
     const instructor = {
         ...foundInstructor,
@@ -61,13 +59,60 @@ exports.post = (req,res) => {
         if (err) {
             return res.send("Write file error")
         }
-
         return res.redirect("/instructors")
     })
 
 }
 
-
 //Update
+exports.edit = (req,res) => {
+    const {id} = req.params
 
+    const foundInstructor = data.instructors.find((instructor) => {
+        return instructor.id == id
+    })
+
+    if (!foundInstructor) {
+        return res.send("Instructor not found")
+    }
+
+    const instructor = {
+        ...foundInstructor,
+        birth: date(foundInstructor.birth)
+    }
+
+    return res.render('instructors/edit', { instructor })
+}
+
+exports.put = (req,res) => {
+    const {id} = req.body
+    let index = 0
+
+    const foundInstructor = data.instructors.find((instructor, foundIndex) => {
+        if (id == instructor.id) {
+            index = foundIndex
+            return true
+        }
+    })
+
+    if (!foundInstructor) {
+        return res.send("Instructor not found")
+    }
+
+    const instructor = {
+        ...foundInstructor,
+        ...req.body,
+        birth: Date.parse(req.body.birth)
+    }
+
+    data.instructors[index] = instructor
+
+    fs.writeFile("data.json",JSON.stringify(data, null, 2), (err) => {
+        if (err) {
+            return res.send("Write file error")
+        } else {
+            return res.redirect(`/instructors/${id}`)
+        }
+    })
+}
 //Delete
